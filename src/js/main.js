965c472360285d9b80bc26df294d487545f0a24a
Andriy Panasiuk <panasiukmail@gmail.com>
2022-10-07 14:23:18 +0300

    added slider and promo

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,7 @@
 import '../style/style.scss'
 import '../style/parts/_header.scss'
-
+import '../style/parts/_promo.scss'
+import '../style/parts/_collections.scss'
 
 import './header.js'
+import './collection-slider.js'
