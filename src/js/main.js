9343f16b08f0165142cc9e23e7c53d7413b0d267
Andriy Panasiuk <panasiukmail@gmail.com>
2022-10-07 16:14:36 +0300

    added services

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ import '../style/parts/_header.scss'
 import '../style/parts/_promo.scss'
 import '../style/parts/_collections.scss'
 import '../style/parts/_products.scss'
+import '../style/parts/_service.scss'
 
 import './header.js'
 import './collection-slider.js'
