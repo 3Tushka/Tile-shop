11fff7c07a7c1c017a4c6a93cccfa32d709c6504
Andriy Panasiuk <panasiukmail@gmail.com>
2022-10-07 15:33:23 +0300

    added product section

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import '../style/style.scss'
 import '../style/parts/_header.scss'
 import '../style/parts/_promo.scss'
 import '../style/parts/_collections.scss'
+import '../style/parts/_products.scss'
 
 import './header.js'
 import './collection-slider.js'
