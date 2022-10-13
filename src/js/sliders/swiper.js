b1b8f2188d0930da35f109540aaa2e5ca9d5c72f
Andriy Panasiuk <panasiukmail@gmail.com>
2022-10-13 14:40:31 +0300

    changes in medias

--- src/js/sliders/swiper.js
+++ src/js/sliders/swiper.js
@@ -7,8 +7,12 @@ export var swiper = new Swiper(".mySwiper", {
         delay: 5000,
     },
     breakpoints: {
-        480: {
+        320: {
             slidesPerView: 1,
+        },
+        600: {
+            slidesPerView: 2,
+            spaceBetween: 20,
         },
         1440: {
             slidesPerView: 3,
